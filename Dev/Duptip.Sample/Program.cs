5ee134f15617e5fb6ca4f45aecfd0264cfea3327
NumAniCloud <num.ani.cloud@gmail.com>
2020-01-22 20:38:39 +0900

    Initial commit.

---
+++ Dev/Duptip.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Duptip.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
